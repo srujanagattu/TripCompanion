54ee80304f0d84e44ba09cba4f2b63f3c0c42c58
Srujana Gattu <S533622@NWMISSOURI.EDU>
2019-04-08 23:49:41 -0500

    All validations done in SignUp.java

--- app/src/main/java/com/example/tripcompanion/SignUp.java
+++ app/src/main/java/com/example/tripcompanion/SignUp.java
@@ -4,12 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
-    SQLHelperClass db ;
+    SQLHelperClass db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +19,8 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.signup);
 
     }
-    public void GoBackToLogin(View v){
+
+    public void GoBackToLogin(View v) {
         EditText uname = (EditText) findViewById(R.id.name);
         EditText email = (EditText) findViewById(R.id.email);
         EditText password = (EditText) findViewById(R.id.password);
@@ -32,15 +34,42 @@ public class SignUp extends AppCompatActivity {
         String pwd = password.getText().toString().trim().toLowerCase();
         String pwd1 = password1.getText().toString().trim().toLowerCase();
         String age1 = age.getText().toString().trim().toLowerCase();
+        int agee = Integer.parseInt(age1);
         String gender1 = gender.getText().toString().trim().toLowerCase();
         String phone = phoneNumber.getText().toString().trim().toLowerCase();
         String location = "Kansas";
-        long a = db.insert(name,gender1,mail,pwd,age1,phone,location);
+        long a = db.insert(name, gender1, mail, pwd, age1, phone, location);
         System.out.println(a);
-        if(validate()) {
-            Intent intent = new Intent(this, Login.class);
-            startActivity(intent);
+        if (name.isEmpty() || mail.isEmpty() || pwd.isEmpty()||pwd1.isEmpty()  )
+
+        {
+
+            Toast.makeText(getApplicationContext(),"Enter valid Name, Email and Password!",Toast.LENGTH_LONG).show();
         }
+
+        else if(!pwd.equals(pwd1)){
+
+                Toast.makeText(getApplicationContext(),"Passwords doesnot match!",Toast.LENGTH_LONG).show();
+            }
+        else if(!(agee>1 && agee<100)){
+  Toast.makeText(getApplicationContext(),"Enter valid age!",Toast.LENGTH_LONG).show();
+      
+    }
+            else {
+                Intent intent = new Intent(this, Login.class);
+                startActivity(intent);
+            }
+
+
+//        if (mail.isEmpty() )
+//        {
+//            Toast.makeText(getApplicationContext(),"Enter valid email !",Toast.LENGTH_LONG).show();
+//        }
+//
+//        else{
+//            Intent intent = new Intent(this, Login.class);
+//            startActivity(intent);
+//        }
 
 
 
@@ -51,37 +80,45 @@ public class SignUp extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         db.openDB();
-        Toast toast = Toast.makeText(getApplicationContext(),"DB is connected",Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(getApplicationContext(), "DB is connected", Toast.LENGTH_SHORT);
         toast.show();
     }
-public boolean validate(){
+
+public boolean validate() {
     EditText uname = (EditText) findViewById(R.id.name);
     EditText email = (EditText) findViewById(R.id.email);
     EditText password = (EditText) findViewById(R.id.password);
     EditText password1 = (EditText) findViewById(R.id.confrimPassword);
     EditText age = (EditText) findViewById(R.id.age);
     String age1 = age.getText().toString().trim().toLowerCase();
-    int agee=Integer.parseInt(age1);
+    int agee = Integer.parseInt(age1);
     String name = uname.getText().toString().trim().toLowerCase();
     String mail = email.getText().toString().trim().toLowerCase();
     String pwd = password.getText().toString().trim().toLowerCase();
     String pwd1 = password1.getText().toString().trim().toLowerCase();
-    if((!name.equals("") && !mail.equals("") && !pwd.equals("")&& !pwd1.equals("")) && (pwd.equals(pwd1)) &&(agee>1 && agee<100)) {
-
-return true;
-    }else if(!pwd.equals(pwd1)){
-        Toast.makeText(getApplicationContext(),"Password doesnot match!!",Toast.LENGTH_LONG).show();
-        return false;
-    }
-    else if(!(agee>1 && agee<100)){
-        Toast.makeText(getApplicationContext(),"Enter valid age!",Toast.LENGTH_LONG).show();
-        return false;
-    }
-    else{
-
-        Toast.makeText(getApplicationContext(),"User Name, Email  and Password are required!!",Toast.LENGTH_LONG).show();
-        return false;
-    }
-
+ return false;
 }
-}//end of class
+
+
+////
+////    else {
+////        Toast.makeText(getApplicationContext(), "Please check name,email,password", Toast.LENGTH_LONG).show();
+////        return false;
+////    }
+////    }else if(!pwd.equals(pwd1)){
+////        Toast.makeText(getApplicationContext(),"Password doesnot match!!",Toast.LENGTH_LONG).show();
+////        return false;
+////    }
+////    else if(!(agee>1 && agee<100)){
+////        Toast.makeText(getApplicationContext(),"Enter valid age!",Toast.LENGTH_LONG).show();
+////        return false;
+////    }
+////    else{
+////
+////        Toast.makeText(getApplicationContext(),"User Name, Email  and Password are required!!",Toast.LENGTH_LONG).show();
+////        return false;
+////    }
+//
+//
+//    }//end of class
+}
