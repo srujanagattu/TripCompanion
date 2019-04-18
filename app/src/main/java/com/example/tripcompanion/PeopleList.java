51ebbcf86f25c28a56a76d9777606c9e0ce3223b
Dristi Marasini <s533985@NWMISSOURI.EDU>
2019-04-18 12:59:29 -0500

    data retrived except own user

--- app/src/main/java/com/example/tripcompanion/PeopleList.java
+++ app/src/main/java/com/example/tripcompanion/PeopleList.java
@@ -27,6 +27,7 @@ public class PeopleList extends AppCompatActivity {
     String city;
     String state;
     ArrayList<String> friends1=new ArrayList<>();
+    String newOb;
     String[] abc={"Sorry you don't have any friends!!"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +38,7 @@ public class PeopleList extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.peoplelist);
-       Intent ini=getIntent();
+        Intent ini=getIntent();
         test=ini.getStringExtra("user_id");
         Parse.initialize(this);
         ParseInstallation.getCurrentInstallation().saveInBackground();
@@ -58,72 +59,76 @@ public class PeopleList extends AppCompatActivity {
 //           tv.setText("Sorry! you do not have any friends!!");
 //       }
 //
- }
- public void abc(){
-     ParseQuery<ParseObject> query = ParseQuery.getQuery("Trip");
-     Log.d("kk","err"+test);
+    }
+    public void abc(){
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Trip");
+        Log.d("kk","err"+test);
 
-     query.whereEqualTo("objectId",test);
-     query.getInBackground(test, new GetCallback<ParseObject>() {
-         @Override
-         public void done(ParseObject ob, com.parse.ParseException e) {
-             if (e == null) {
+        query.whereEqualTo("objectId",test);
+        query.getInBackground(test, new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject ob, com.parse.ParseException e) {
+                if (e == null) {
 
-           city=ob.getString("city");
-          state=ob.getString("state");
-                 Log.d("test","err"+city);
-                 Log.d("test","err"+state);
-
-
+                    city=ob.getString("city");
+                    state=ob.getString("state");
+                    Log.d("test","err"+city);
+                    Log.d("test","err"+state);
 
 
 
-             }
-         }
-     });
-     tryy();
- }
 
-public void tryy(){
-    final ParseQuery<ParseObject> query = ParseQuery.getQuery("Trip");
+
+                }
+            }
+        });
+        tryy();
+    }
+
+    public void tryy(){
+        final ParseQuery<ParseObject> query = ParseQuery.getQuery("Trip");
 // Fetches all the Movie objects
-    query.findInBackground(new FindCallback<ParseObject>() {
-        @Override
-        public void done(List<ParseObject> tripList, ParseException e) {
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> tripList, ParseException e) {
 
-            String ct;
-            String st;
-            if (e == null) {
-                Log.d("Parse", "Trips retrieved:" + tripList.size());
+                String ct;
+                String st;
+                if (e == null) {
+                    Log.d("Parse", "Trips retrieved:" + tripList.size());
 // Do something with the found objects
-                for (int i = 0; i < tripList.size(); i++) {
-                    ct= (String)tripList.get(i).get("city");
-                    st=(String)tripList.get(i).get("state");
+                    for (int i = 0; i < tripList.size(); i++) {
+                        ct= (String)tripList.get(i).get("city");
+                        st=(String)tripList.get(i).get("state");
+                        newOb=tripList.get(i).getObjectId();
+                        Log.d("Parse", "obj retrieved:" +newOb);
 
-                    if(city.equals(ct) && state.equals(st) ) {
-                        String name= (String)tripList.get(i).get("name");
-                        friends1.add(name);
+                        if(city.equals(ct) && state.equals(st) ) {
+                            String name= (String)tripList.get(i).get("name");
+                            if(!test.equals(newOb)) {
+                                friends1.add(name);
+                            }
 
+
+                        }
 
                     }
+                    Log.d ("Df","Dfd"+friends1.size());
+                    if(friends1.size()!=0){
+                        showMem();
+                    }
+                    else{
+                        noMem();
+                    }
 
+                } else {
+                    Log.d("Parse", "Error: " + e.getMessage());
                 }
-                Log.d ("Df","Dfd"+friends1.size());
-                if(friends1.size()!=0){
-                    t();
-                }
-                else{
-                    y();
-                }
-
-            } else {
-                Log.d("Parse", "Error: " + e.getMessage());
             }
-        }
-    });
+        });
 
     }
-public void t(){
+    public void showMem(){
 //
 //    ListAdapter ad=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,abc);
 //    ListView lv=(ListView)findViewById(R.id.listView);
@@ -132,15 +137,15 @@ public void t(){
 //
 ////        else{
 ////
-    ArrayAdapter<String> ad =
-            new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friends1);
-    ListView lv = (ListView) findViewById(R.id.listView);
-    lv.setAdapter(ad);
+        ArrayAdapter<String> ad =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friends1);
+        ListView lv = (ListView) findViewById(R.id.listView);
+        lv.setAdapter(ad);
 //        }
-}
-public void y(){
-    ListAdapter ad=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,abc);
-    ListView lv=(ListView)findViewById(R.id.listView);
-    lv.setAdapter(ad);
-}
+    }
+    public void noMem(){
+        ListAdapter ad=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,abc);
+        ListView lv=(ListView)findViewById(R.id.listView);
+        lv.setAdapter(ad);
+    }
 }
