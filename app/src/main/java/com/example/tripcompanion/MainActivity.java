ccede2903be464ef8231b91329d4f0a4daf1a67f
karthik <s533488@nwmissouri.edu>
2019-04-28 18:18:40 -0500

    Updated two lines in the layout/activity_login.xml and MainActivity.java

--- app/src/main/java/com/example/tripcompanion/MainActivity.java
+++ app/src/main/java/com/example/tripcompanion/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
     public void secondAct(View v){
-        Intent intent=new Intent(this,TripOptions.class);
+        Intent intent=new Intent(this,Login.class);
         startActivity(intent);
     }
 
